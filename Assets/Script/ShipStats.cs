926d5bb42ec26f26fedff2bf98268c9aefd60240
EthanDavies <113374445+Deenhan@users.noreply.github.com>
2024-05-28 16:09:35 +0100

    same as before

---
+++ Assets/Script/ShipStats.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipStats : MonoBehaviour
+{
+
+    public static float maxBattery;
+    public static float batteryDecay;
+    public static float moveSpeed;
+    public static float limitVelocity;
+    public static float oxygenDecay;
+    public static float oxygenRegen;
+    public static float maxOxygen;
+    public static float moneyMultiplier;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
